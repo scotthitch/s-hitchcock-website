5adcf95d19b407ce5c24b8e42cc1086938d2cd2d
Scott Hitchcock <scotthitch5@gmail.com>
2024-02-20 12:19:23 +1300

    chore: now rendering a p5 script within p5scriptdescriptor

--- src/components/P5ScriptDescriptor.vue
+++ src/components/P5ScriptDescriptor.vue
@@ -5,16 +5,19 @@ const props = defineProps({
     title: String,
     description: String,
     usageInstructions: String,
+    p5Script: Object,
 })
 
 </script>
 
 <template>
-<div>{{ props.title }}</div>
-<div>{{ props.description }}</div>
-<div>{{ props.usageInstructions }}</div>
-
-</template>
+    <div class="flex flex-col items-center text-center justify-center h-screen">
+      <div>{{ props.title }}</div>
+      <div>{{ props.description }}</div>
+      <component :is="p5Script" :screenWidth="400" :screenHeight="400" :scriptIsPlaying="true"></component>
+      <div>{{ props.usageInstructions }}</div>
+    </div>
+  </template>
 
 <style scoped>  
 </style>
