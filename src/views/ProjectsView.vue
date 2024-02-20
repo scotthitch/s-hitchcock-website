62a28292e2e1ea3f6c0aced6b718f013dca9dc5e
Scott Hitchcock <scotthitch5@gmail.com>
2024-02-20 16:10:16 +1300

    feat: now a single project is rendered at a time

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
 import KaleidoscopeProject from '../p5Projects/Kaleidoscope/KaleidoscopeProject.vue';
 import BallCascadeProject from '../p5Projects/BallCascade/BallCascadeProject.vue';
+import { ref, shallowRef } from 'vue'
+
+
+const projectSelectIndex = ref(0)
+const projects = shallowRef([KaleidoscopeProject, BallCascadeProject]);
+
+const incrementProjectSelect = () => {
+  projectSelectIndex.value = Math.min(projectSelectIndex.value+1, projects.value.length-1);
+}
+
+const decrementProjectSelect = () => {
+  projectSelectIndex.value = Math.max(projectSelectIndex.value-1, 0);
+}
 
 </script>
 
+
 <template>
   <div>
-    <KaleidoscopeProject />
-    <BallCascadeProject />
+    <div class="flex flex-col items-center text-center justify-center py-[5rem]">
+      
+      <button :disabled="projectSelectIndex === 0" @click="decrementProjectSelect()">Previous</button>
+      <component :is="projects[projectSelectIndex]"></component>
+      <button :disabled="projectSelectIndex === projects.length-1" @click="incrementProjectSelect()">Next</button>
+      
+    </div>
   </div>
 </template>
