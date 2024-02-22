a387590fd2c480c4e96869b88f917382e3d77de1
Scott Hitchcock <scotthitch5@gmail.com>
2024-02-23 08:54:28 +1300

    feat: added cv to resume view also with download button

---
+++ src/components/DownloadAsset.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+    assetPath: string,
+    assetFileType: string
+    downloadName: string,
+}>();
+
+</script>
+
+<template>
+    <a :href="props.assetPath" :download="`${downloadName}.${assetFileType}`">
+        <button>
+            Download
+        </button>
+    </a>
+</template>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
+import DownloadAsset from '../components/DownloadAsset.vue';
+import cvURL from '../assets/scott_hitchcock_cv.pdf'
+
 </script>
 
 <template>
-  <div>This is the Resume View</div>
+  <div class="p-6 md:py-10 drop-shadow-md">
+    
+      <img src="../assets/scott_hitchcock_cv-1.png">
+      <img class="mt-2" src="../assets/scott_hitchcock_cv-2.png">
+      <DownloadAsset
+        :assetPath="cvURL"
+        assetFileType="pdf"
+        downloadName="Scott Hitchcock CV"
+        />
+      
+  </div>
+
 </template>
