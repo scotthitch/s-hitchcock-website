c9731388f0e0caeaf161777c3c283be6ee5ee974
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-01 08:40:07 +1300

    feat: make the projects full screen with text overlayed

--- src/components/P5Project.vue
+++ src/components/P5Project.vue
@@ -22,9 +22,10 @@ const handleResize = () => {
 }
 
 const calculateCanvasDimensions = (): ScreenDimensions => {
-    const width = Math.min(props.maxCanvasDimensions.width, liveScreenDimensions.value.width*0.95)
-    const height = Math.min(props.maxCanvasDimensions.height, liveScreenDimensions.value.width*0.95*canvasAspectRatio.value)
-
+    // const width = Math.min(props.maxCanvasDimensions.width, liveScreenDimensions.value.width*0.95)
+    // const height = Math.min(props.maxCanvasDimensions.height, liveScreenDimensions.value.width*0.95*canvasAspectRatio.value)
+    const width = liveScreenDimensions.value.width;
+    const height =liveScreenDimensions.value.height;
     return {width: width, height: height}
 }
 
@@ -39,16 +40,30 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="flex flex-col items-center text-center place-content-center h-screen snap-always snap-center">
-        <p class="text-3xl font-bold">{{ props.title }}</p>
-        <p>{{ props.description }}</p>
-        <P5Canvas 
+    <div class="relative h-screen snap-always snap-center">
+        <div class="absolute">
+
+            <P5Canvas 
             :key="p5CanvasComponentKey"
             :scriptID="props.scriptID"
             :script="props.script"
             :screenDimensions="calculateCanvasDimensions()"
-        />
-        <p>{{ props.usageInstructions }}</p>
+            />        
+        </div>
+        <div class="absolute bottom-0 left-0">
+            <p class="text-8xl font-bold text-white opacity-75">{{ props.title }}</p>
+        </div>
+        
+        <div class="absolute bottom-0 right-0">
+            <p class="text-2xl font-bold text-white opacity-75">{{ props.description }}</p>
+        </div>
+
+        <div class="absolute top-0 right-0">
+            <p class="text-4xl font-bold text-white opacity-75">{{ props.usageInstructions }}</p>
+        </div>
+
+        <!-- <p>{{ props.description }}</p> -->
+        <!-- <p>{{ props.usageInstructions }}</p> -->
     </div>
 </template>
 
