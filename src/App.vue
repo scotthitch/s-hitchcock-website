cf08d5c5a004697dc63b55959e0083aa4fbec157
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-01 21:31:26 +1300

    refactor: update import for navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import NavBar from './components/Navbar.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
