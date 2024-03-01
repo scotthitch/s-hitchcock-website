c6607bdcf7e37febfc8f11a1ca843ab89a0d63ed
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-01 18:23:18 +1300

    feat: add max width to resume

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -5,7 +5,7 @@ import cvURL from '../assets/scott_hitchcock_cv.pdf';
 
 <template>
   <div class="flex flex-col items-center p-2 sm:py-6">
-    <div class="flex flex-col gap-2 sm:w-1/2 sm:min-w-[600px]">
+    <div class="flex flex-col gap-2 sm:w-1/2 sm:min-w-[600px] max-w-[700px]">
       <img src="../assets/scott_hitchcock_cv-1.png" class="mx-auto shadow-md">
       <img src="../assets/scott_hitchcock_cv-2.png" class="mx-auto shadow-md">
       <div class="text-end">
