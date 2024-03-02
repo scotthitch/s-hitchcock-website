460b111ba5a0a4433e2008693af49001c8567427
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-02 18:17:54 +1300

    feat: navbar operating as it should
    navbar will only close if clicked off or if a link is taken. otherwise it will stay open

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,29 +3,58 @@ import { ref, onMounted, onUnmounted } from 'vue';
 
 const isNavBarExpanded = ref(false);
 
+const navBarRoutes = [
+    {
+        to: "/",
+        id: "home",
+        title: 'Home'
+    },
+    {
+        to: "/projects",
+        id: "projects",
+        title: "Projects"
+    },
+    {
+        to: "/resume",
+        id: "resume",
+        title: "Resume"
+    }
+]
 
+const elementIsNavRoute = (id: string): boolean => {
+    for (const route of navBarRoutes) {
+        if (route.id === id) {
+            return true; // Word found
+        }
+    }
+    return false; // Word not found
+}
 
 const handlePageClick = (event: MouseEvent) => {
-    // If the navbar is already expanded then just close it
-    if (isNavBarExpanded.value === true) {
-        isNavBarExpanded.value = false;
-        return;
-    }
-    
     const navBarElement = document.getElementById('navbar');
     const clickedElement = event.target as HTMLElement
-    
+
+    if (elementIsNavRoute(clickedElement.id)) {
+        isNavBarExpanded.value = false;
+        return
+    }
+
     // Null element
     if (navBarElement === null || clickedElement === null) {
         return;
     }
-
+    
     // If the click isn't on the element
     if (!navBarElement.contains(clickedElement) && clickedElement !== navBarElement) {
         isNavBarExpanded.value = false;
         return;
     }
 
+    // If the navbar is already expanded then just close it
+    if (isNavBarExpanded.value === true) {
+        return;
+    }
+    
     // Click was on navbar
     isNavBarExpanded.value = true;
     return
@@ -44,12 +73,12 @@ onUnmounted(() => {
 
 <template>
     <div class="fixed z-30 top-0 bottom-0 left-0 flex items-center" id="navbar">
-      <button
+      <div
         class="rounded-r-2xl flex flex-col justify-center items-center nav-bar-background"
         :class="isNavBarExpanded ? 
                 'h-screen w-[40vw] sm:w-[25vw] lg:w-[20vw]':
                 'h-24 w-12'"
-        style="background-color: rgba(0, 0, 0, 0.55);"
+        style="background-color: rgba(0, 0, 0, 0.65);"
         >
             <Transition name="arrow">
                 <img v-if="!isNavBarExpanded"
@@ -60,25 +89,23 @@ onUnmounted(() => {
             </Transition>
             <Transition name="arrow">
                 <div v-if="isNavBarExpanded" class="flex flex-col gap-12 w-full px-8 place-items-end h-screen">
-                    <div class="basis-1/3 py-8">
+                    <div class="absolute py-6">
                         <p class="text-white text-xs sm:text-xl font-bold">
                             Scott Hitchcock
-                        </p>           
-                    </div>
-                    <div class="text-white text-3xl font-semibold">
-                        <router-link to="/">Home</router-link>
-                    </div>
-                    <div class="text-white text-3xl font-semibold">
-                        <router-link to="/projects">Projects</router-link>
-                    </div>
-                    <div class="text-white text-3xl font-semibold">
-                        <router-link to="/resume">Resume</router-link>
+                        </p> 
+                    </div>          
+                    <div class="basis-1/3"></div>
+                    <div v-for="(navBarRoute, index) in navBarRoutes" v-bind:key="index" class="text-white text-3xl font-semibold">
+                        <router-link
+                            :to="navBarRoute.to"
+                            :id="navBarRoute.id"
+                        >{{ navBarRoute.title }}</router-link>
                     </div>
                     <div class="basis-1/3"></div>
                 </div>
             </Transition>
             
-        </button>
+        </div>
     </div>
   </template>
 
