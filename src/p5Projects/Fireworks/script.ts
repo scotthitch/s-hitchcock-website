3276d6eb8a3c446ed7449b6ef3b17f710834d303
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-04 11:17:37 +1300

    refactor: removed hadexploded property

--- src/p5Projects/Fireworks/script.ts
+++ src/p5Projects/Fireworks/script.ts
@@ -4,7 +4,7 @@ import P5 from 'p5' // Package from npm
 const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
     const s = (p5Instance: P5): void => {
 
-        const GRAVITY = p5Instance.createVector(0, -0.25)
+        const GRAVITY = p5Instance.createVector(0, -0.2)
         const missiles: Missile[] = [];
 
         p5Instance.setup = () => {
@@ -19,8 +19,9 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
             missile.fly()
             missile.draw()
 
-            if (missile.hasExploded) {
-              generateExplodedFireworks(missile.position, missile.velocity)
+            // If the missile has exploded then generate it's children and remove it
+            if (missile.readyToExplode()) {
+              generateExplodedFireworks(30, missile.position)
               missilesArray.splice(index, 1)
               return
             }
@@ -32,12 +33,12 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
           })
         }
 
-        const generateExplodedFireworks = (parentPosition: P5.Vector, parentVelocity: P5.Vector) => {
-          const n = 12;
-          const angleDelta = p5Instance.TWO_PI / n;
-          for (let i: number = 0; i < n; i++) {
+        const generateExplodedFireworks = (nFireworks: number, parentPosition: P5.Vector) => {
+          // const n = 12;
+          const angleDelta = p5Instance.TWO_PI / nFireworks;
+          for (let i: number = 0; i < nFireworks; i++) {
             const angle = angleDelta * i * p5Instance.random(1, 1.08);
-            const missileVelocity = P5.Vector.fromAngle(angle, 2.6).mult(p5Instance.random(1, 1.08));
+            const missileVelocity = P5.Vector.fromAngle(angle, 2.6).mult(p5Instance.random(0.1, 1.2));
             missiles.push(new Missile(parentPosition.copy(), missileVelocity, 1000))
           }          
         }
@@ -91,23 +92,22 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
           position: P5.Vector;
           velocity: P5.Vector;
           explodeCountdown: number;
-          hasExploded: boolean;
+
 
           constructor(initialPosition: P5.Vector, initialVelocity: P5.Vector, explodeTime: number) {
             this.position = initialPosition;
             this.velocity = initialVelocity;
             this.explodeCountdown = explodeTime;
-            this.hasExploded = false;
           }
 
           fly() {
             this.velocity.add(GRAVITY);
             this.position.add(this.velocity);
             this.explodeCountdown--;
+          }
 
-            if (!this.hasExploded && this.explodeCountdown <= 0) {
-              this.hasExploded = true;
-            }
+          readyToExplode() {
+            return this.explodeCountdown <= 0;
           }
 
           belowScreenBottom(screenBottom: number) {
