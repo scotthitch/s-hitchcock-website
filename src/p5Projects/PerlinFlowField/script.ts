1d6cc4dd387946ab624f094882bf98b18cd46215
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-13 13:25:11 +1300

    refactor: fix perlinFlowField for typescript

--- src/p5Projects/PerlinFlowField/script.ts
+++ src/p5Projects/PerlinFlowField/script.ts
@@ -1,8 +1,8 @@
 import type { ScreenDimensions, p5ScriptInnerFunction } from '../../types'
-import P5, { Vector } from 'p5' // Package from npm
+import P5 from 'p5' // Package from npm
 
 const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
-    const s = (p5: P5): void => {
+    const s = (p5Instance: P5): void => {
         const GRID_ITEM_WIDTH = screenDimensions.width/400;
         const FORCE_SCALE_FACTOR = 50;
         const N_POINTS = 10000;
@@ -10,7 +10,7 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
         const CURL = 6.3;
         const POINT_VELOCITY_LIMIT = 1;
         const POINT_OPACITY = 3; // %
-        const RANDOM_BASE_OFFSET = p5.random(2*p5.TWO_PI);
+        const RANDOM_BASE_OFFSET = p5Instance.random(2*p5Instance.TWO_PI);
         // const RANDOM_BASE_OFFSET = 0;
         // const BACKGROUND_COLOUR = "#FF1B1C";
         // const PRIMARY_COLOUR = "#FFE099";
@@ -20,18 +20,15 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
 
         const POINT_SIZE = Math.min(screenDimensions.height, screenDimensions.width) / 200;
         
-        let grassPatches = [];
-        let points = [];
+        const grassPatches: GrassPatch[][] = [];
+        const points: Point[] = [];
         
-        function generatePerlinForcePatch(i, j) {
-            let pos = p5.createVector(i * GRID_ITEM_WIDTH, j * GRID_ITEM_WIDTH);
+        function generatePerlinForcePatch(i: number, j: number) {
+            const pos = p5Instance.createVector(i * GRID_ITEM_WIDTH, j * GRID_ITEM_WIDTH);
             
-            let angle = p5.noise(i * NOISE_SCALE, j * NOISE_SCALE) * CURL + RANDOM_BASE_OFFSET; // Adjust noise parameters
-            let force = Vector.fromAngle(angle);
-            // p5.fromAng÷
-            // Vector.fromAngle
-            // p5.Vec
-            // p5.vec
+            const angle = p5Instance.noise(i * NOISE_SCALE, j * NOISE_SCALE) * CURL + RANDOM_BASE_OFFSET; // Adjust noise parameters
+            const force = P5.Vector.fromAngle(angle);
+        
             force.setMag(FORCE_SCALE_FACTOR);
             
             grassPatches[i][j] = (
@@ -39,33 +36,33 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
             );
         }
         
-        p5.setup = () => {
-            p5.createCanvas(screenDimensions.width, screenDimensions.height);
-            p5.background(BACKGROUND_COLOUR);
+        p5Instance.setup = () => {
+            p5Instance.createCanvas(screenDimensions.width, screenDimensions.height);
+            p5Instance.background(BACKGROUND_COLOUR);
         
             // Generate points
             for (let i = 0; i < N_POINTS; i++) {
-                const screenBounds = p5.createVector(p5.width, p5.height);
-                const pointPos = p5.createVector(Math.random() * p5.width, Math.random() * p5.height);
+                const screenBounds = p5Instance.createVector(p5Instance.width, p5Instance.height);
+                const pointPos = p5Instance.createVector(Math.random() * p5Instance.width, Math.random() * p5Instance.height);
                 points.push(new Point(pointPos, screenBounds));
             }
         
             // Generate grass patches
-            for (let i = 0; i < p5.width/GRID_ITEM_WIDTH; i++) 
+            for (let i = 0; i < p5Instance.width/GRID_ITEM_WIDTH; i++) 
             {
                 grassPatches[i] = [];
-                for (let j = 0; j < p5.height/GRID_ITEM_WIDTH; j++) 
+                for (let j = 0; j < p5Instance.height/GRID_ITEM_WIDTH; j++) 
                 {
                     generatePerlinForcePatch(i, j);
                 }
             }
         }
         
-        p5.draw = () => {
+        p5Instance.draw = () => {
             // background(30);
         
             points.forEach((point) => {
-                let [i, j] = point.positionAsIndex(GRID_ITEM_WIDTH);
+                const [i, j] = point.positionAsIndex(GRID_ITEM_WIDTH);
                 point.updatePosition(grassPatches[i][j].getForce())
                 point.display();
             })
@@ -73,11 +70,17 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
         }
 
         class Point {
-            constructor(pos, screenBounds) {
+            originalPos: P5.Vector
+            pos: P5.Vector
+            vel: P5.Vector
+            acc: P5.Vector
+            screenBounds: P5.Vector
+
+            constructor(pos: P5.Vector, screenBounds: P5.Vector) {
                 this.originalPos = pos.copy();
                 this.pos = pos;
-                this.vel = p5.createVector(0, 0);
-                this.acc = p5.createVector(0, 0);
+                this.vel = p5Instance.createVector(0, 0);
+                this.acc = p5Instance.createVector(0, 0);
                 this.screenBounds = screenBounds;
             }
         
@@ -91,12 +94,13 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
             resetPoint() {
                 // this.pos = this.originalPos.copy();
         
-                this.pos = p5.createVector(Math.random() * this.screenBounds.x, Math.random() * this.screenBounds.y);
-                this.vel = p5.createVector(0, 0);
-                this.acc = p5.createVector(0, 0);
+                this.pos = p5Instance.createVector(Math.random() * this.screenBounds.x, Math.random() * this.screenBounds.y);
+                this.vel = p5Instance.createVector(0, 0);
+                this.acc = p5Instance.createVector(0, 0);
             }
         
-            updatePosition(force) {
+            updatePosition(force: P5.Vector) {
+                // TODO: units don't match
                 this.acc = force;
                 this.vel.add(this.acc);
                 this.vel.limit(POINT_VELOCITY_LIMIT)
@@ -109,62 +113,35 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
             }
         
             display() {
-                // let col = ;
-                // col.setAlpha
-                const pointColour = p5.color(PRIMARY_COLOUR);
+                const pointColour = p5Instance.color(PRIMARY_COLOUR);
                 pointColour.setAlpha(POINT_OPACITY);
-                p5.fill(pointColour);
-                p5.noStroke();
-                p5.ellipse(this.pos.x, this.pos.y, POINT_SIZE);
+                p5Instance.fill(pointColour);
+                p5Instance.noStroke();
+                p5Instance.ellipse(this.pos.x, this.pos.y, POINT_SIZE);
             }
         
             // need some bounds checker
-            positionAsIndex(scaleFactor) {
-                let i = Math.floor(this.pos.x / scaleFactor);
-                let j = Math.floor(this.pos.y / scaleFactor);
+            positionAsIndex(scaleFactor: number) {
+                const i = Math.floor(this.pos.x / scaleFactor);
+                const j = Math.floor(this.pos.y / scaleFactor);
         
                 return [i, j]
             }
         }
 
         class GrassPatch {
-            constructor(pos, width, force, forceScaleFactor) {
+            pos: P5.Vector
+            width: number
+            force: P5.Vector
+            forceScaleFactor: number
+
+            constructor(pos: P5.Vector, width: number, force: P5.Vector, forceScaleFactor: number) {
                 this.pos = pos;
                 this.width = width;
                 this.force = force;
                 this.forceScaleFactor = forceScaleFactor;
             }
-        
-            displayWind() {
-                // Calculate the line
-                let forceArm = p5.Vector.mult(this.force, this.width/(2*this.forceScaleFactor));
-        
-                let xWindBase = this.pos.x + this.width/2;
-                let yWindBase = this.pos.y + this.width/2;
-                let xWindTip = xWindBase + forceArm.x;
-                let yWindTip = yWindBase + forceArm.y;
-        
-                // Display
-                p5.noFill();
-                p5.stroke(255);
-                p5.strokeWeight(this.force.mag() * 50);
-                p5.line(xWindBase, yWindBase, xWindTip, yWindTip);
-            }
-        
-            displayGrid() {
-                p5.noFill();
-                p5.stroke(255, 255, 255);
-                p5.strokeWeight(1)
-                p5.rect(this.pos.x, this.pos.y, this.width, this.width);
-            }
-        
-            indicateGrid() {
-                p5.noFill();
-                p5.stroke(255, 0, 0);
-                p5.strokeWeight(1)
-                p5.rect(this.pos.x, this.pos.y, this.width, this.width);
-            }
-        
+            
             getForce() {
                 return this.force;
             }
