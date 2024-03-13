09210ec6f7f1d9a2b0025bd8ac3f3550d2af17f1
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-13 13:41:10 +1300

    refactor: fix joy division proj. for typescript

--- src/p5Projects/JoyDivisionLines/script.ts
+++ src/p5Projects/JoyDivisionLines/script.ts
@@ -9,45 +9,35 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
         const Y_BOTTOM_DRAWING_BOUNDS = screenDimensions.height - Y_TOP_DRAWING_BOUNDS;
         const ONE_OVER_ROOT_2_PI = 1 / Math.sqrt(Math.PI);
 
-        let xStep;
-        let yStep;
-        let mu = 250;
-        let preMu = mu;
-        let sd = 40;
-        let sdMin;
-        let sdMax;
-        let preSd = sd;
-        let magn = 400;
-        let preMagn = magn;
+        const X_STEP = (X_RIGHT_DRAWING_BOUNDS - X_LEFT_DRAWING_BOUNDS) / 100;
+        const Y_STEP = (Y_BOTTOM_DRAWING_BOUNDS - Y_TOP_DRAWING_BOUNDS) / 60;
+        const MIN_STANDARD_DEVIATION = screenDimensions.height / 45;
+        const MAX_STANDARD_DEVIATION = screenDimensions.height / 6;
+        let MU = 250;
+        let PREVIOUS_MU = MU;
+        let STANDARD_DEVIATION = 40;
+        let preSd = STANDARD_DEVIATION;
+        const magn = 800;
         let isLooping = 1;
 
         p5.setup = () => {
-            p5.createCanvas(screenDimensions.width, screenDimensions.height);       
-
-            xStep = (X_RIGHT_DRAWING_BOUNDS - X_LEFT_DRAWING_BOUNDS) / 100;
-            yStep = (Y_BOTTOM_DRAWING_BOUNDS - Y_TOP_DRAWING_BOUNDS) / 60;
-
-
-            sdMin = p5.height / 45;
-            sdMax = p5.height / 6;
-
+            p5.createCanvas(screenDimensions.width, screenDimensions.height);
             p5.strokeWeight(p5.height / 350);
             firstRender();
         }
 
         function firstRender() {
             p5.background(0, 32, 63);
-            for (let j = Y_TOP_DRAWING_BOUNDS; j <= Y_BOTTOM_DRAWING_BOUNDS; j += yStep) {
-                let y1 = j;
-                let x1 = X_LEFT_DRAWING_BOUNDS;
-
-
+            for (let j = Y_TOP_DRAWING_BOUNDS; j <= Y_BOTTOM_DRAWING_BOUNDS; j += Y_STEP) {
+                const y1 = j;
+                const x1 = X_LEFT_DRAWING_BOUNDS;
 
                 p5.beginShape();
 
                 p5.curveVertex(x1, y1)
-                let yVal, xVal;
-                for (let i = X_LEFT_DRAWING_BOUNDS; i <= X_RIGHT_DRAWING_BOUNDS; i += xStep) {
+                let xVal = 0;
+                let yVal = 0;
+                for (let i = X_LEFT_DRAWING_BOUNDS; i <= X_RIGHT_DRAWING_BOUNDS; i += X_STEP) {
 
                     yVal = deltaY(i) + j
                     xVal = i;
@@ -61,7 +51,6 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
                     }
 
                 }
-
                 p5.curveVertex(xVal, yVal)
                 p5.endShape();
 
@@ -71,36 +60,36 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
 
         p5.draw = () => {
 
-            mu = p5.mouseX;
-            if (mu != preMu) {
+            MU = p5.mouseX;
+            if (MU != PREVIOUS_MU) {
                 muChanged();
             }
 
-            sd = p5.map(p5.mouseY, 0, p5.height, sdMin, sdMax, true);
-            if (sd != preSd) {
+            STANDARD_DEVIATION = p5.map(p5.mouseY, 0, p5.height, MIN_STANDARD_DEVIATION, MAX_STANDARD_DEVIATION, true);
+            if (STANDARD_DEVIATION != preSd) {
                 sdChanged()
             }
         }
 
         function muChanged() {
-            preMu = mu;
+            PREVIOUS_MU = MU;
             firstRender();
         }
 
         function sdChanged() {
-            preSd = sd;
+            preSd = STANDARD_DEVIATION;
             firstRender();
         }
 
 
 
-        function deltaY(x) {
+        function deltaY(x: number) {
             let sign = 1;
             if (Math.random() > 0.5) {
                 sign *= -1;
             }
-            let power = Math.exp(-((x - mu) * (x - mu)) / (2 * sd * sd))
-            let y = ((power * ONE_OVER_ROOT_2_PI) / sd) * sign
+            const power = Math.exp(-((x - MU) * (x - MU)) / (2 * STANDARD_DEVIATION * STANDARD_DEVIATION))
+            let y = ((power * ONE_OVER_ROOT_2_PI) / STANDARD_DEVIATION) * sign
             y *= magn;
             y *= p5.random(0.4, 1.4)
             return y;
@@ -116,31 +105,6 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
                 isLooping = 1;
             }
         }
-
-        p5.keyPressed = () => {
-            if (p5.keyCode === p5.UP_ARROW) {
-                if (magn === 1000) {
-                    
-                } else {
-                    magn = Math.min(1000, magn + 50);
-                    firstRender();
-                }
-            } else if (p5.keyCode === p5.DOWN_ARROW) {
-                if (magn === 0) {
-
-                } else {
-                    magn = Math.max(0, magn - 50);
-                    firstRender();
-                }
-            }
-        }
-
-
-
-
-
-
-
     }
     return s;
 }
