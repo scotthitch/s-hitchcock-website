4398b94d1116c2e3e976276693347e9ef723b74e
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-19 11:31:43 +1300

    chore: remove tailwind config from gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'cv-blue': '#0F365B'
+      }
+    },
+  },
+  plugins: [],
+}
+
