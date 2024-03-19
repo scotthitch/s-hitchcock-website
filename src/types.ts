54a2e3e4befe9501a0f7c5ad950d31969b505915
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-19 20:26:58 +1300

    chore: add quadratic roots type and class

--- src/types.ts
+++ src/types.ts
@@ -32,3 +32,8 @@ export interface P5ProjectProps {
 
 export type p5ScriptInnerFunction = (p5: P5) => void;
 export type p5ScriptWrapperFunction = (screenDimensions: ScreenDimensions) => p5ScriptInnerFunction;
+
+export interface QuadraticRoot {
+    real: number,
+    imaginary: number
+}
