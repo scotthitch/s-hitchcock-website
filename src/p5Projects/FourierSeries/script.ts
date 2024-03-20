ce39a562575e7779addfa4b31857eeb605a4df4e
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-21 10:24:59 +1300

    feat: make FS scale with screen size

--- src/p5Projects/FourierSeries/script.ts
+++ src/p5Projects/FourierSeries/script.ts
@@ -6,13 +6,13 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
 
         const N_ORBITERS_MAX = 101;
         const orbiters: Orbiter[] = [];
-        const AMPLITUDE_COEFFICIENT = 90;
+        const AMPLITUDE_COEFFICIENT = p5Instance.max(screenDimensions.width/20, 40);
         const FREQUENCY_COEFFICIENT = 1 / 40;
         let yVals: number[] = [];
         let origin: P5.Vector;
-        const NODE_RADIUS = 8; // TODO: make this a fnc of screensize
-        const WAVE_MOVE_SPEED = 2.5; // TODO: made this a fnc of viewwidth
-        const WAVE_X_SHIFT = 600; // TODO: made this a fnc of viewwidth
+        const NODE_RADIUS = p5Instance.max(screenDimensions.width/230, 4.5); // TODO: make this a fnc of screensize
+        const WAVE_MOVE_SPEED = screenDimensions.width/800;
+        const WAVE_X_SHIFT = p5Instance.max(screenDimensions.width/2.5, 200);
         let nTermsInSeries = 1;
         let nPreviousTermsInSeries = nTermsInSeries;
         const MAX_LEN = 700;
@@ -31,8 +31,6 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
 
         p5Instance.draw = () => {
             p5Instance.background(247, 37, 133);
-
-            // aChoose = aSlider.value();
             orbiters[0].orbit(origin.x, origin.y);
             orbiters[0].render();
             p5Instance.fill(250, 243, 221)
@@ -49,24 +47,21 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
             orbiters[nTermsInSeries - 1].render(250, 243, 221)
             const finalY = orbiters[nTermsInSeries - 1].y
             yVals.unshift(finalY)
-            p5Instance.strokeWeight(1)
+            p5Instance.strokeWeight(NODE_RADIUS*0.4)
             p5Instance.stroke(250, 243, 221);
             p5Instance.line(orbiters[nTermsInSeries - 1].x, finalY, WAVE_X_SHIFT, finalY)
             p5Instance.beginShape();
             p5Instance.noFill();
             for (let j = 0; j < yVals.length; j++) {
-                p5Instance.strokeWeight(4)
+                p5Instance.strokeWeight(NODE_RADIUS*0.75)
                 p5Instance.vertex(WAVE_MOVE_SPEED * j + WAVE_X_SHIFT, yVals[j])
             }
 
             p5Instance.endShape();
             p5Instance.strokeWeight(0.4)
             p5Instance.ellipse(origin.x, origin.y, orbiters[0].amplitude * 2)
-            console.log(orbiters[0].amplitude * 2)
             for (let i = 1; i < nTermsInSeries - 1; i++) {
-                // fill(255);
                 p5Instance.ellipse(orbiters[i].x, orbiters[i].y, orbiters[i + 1].amplitude * 2);
-                // console.log(orbiters[i+1].atude)
             }
 
             if (yVals.length > MAX_LEN) {
@@ -81,7 +76,6 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
         }
 
         p5Instance.keyPressed = () => {
-            console.log(nTermsInSeries)
             if (p5Instance.keyCode === p5Instance.UP_ARROW) {
                 nTermsInSeries = Math.min(N_ORBITERS_MAX, nTermsInSeries + 2);
             } else if (p5Instance.keyCode === p5Instance.DOWN_ARROW) {
@@ -122,9 +116,8 @@ const script = (screenDimensions: ScreenDimensions): p5ScriptInnerFunction => {
                 p5Instance.strokeWeight(3)
                 p5Instance.line(this.refX, this.refY, this.x, this.y)
                 p5Instance.fill(r, g, b);
-                p5Instance.ellipse(this.x, this.y, this.r - 2)
+                p5Instance.ellipse(this.x, this.y, this.r)
                 p5Instance.noFill();
-                p5Instance.strokeWeight(0.7);        
             }
         }
     }
