4ed48adc5517e0b46007a96fa9637c1d72927964
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-21 14:43:36 +1300

    refactor: reformat with prettier with tabs at 4 spaces

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,42 +1,50 @@
 <script setup lang="ts">
-import RadialProject from '../p5Projects/Radial/RadialProject.vue';
-import BallCascadeProject from '../p5Projects/BallCascade/BallCascadeProject.vue';
-import JoyDivisionLinesProject from '../p5Projects/JoyDivisionLines/JoyDivisionLinesProject.vue';
+import RadialProject from '../p5Projects/Radial/RadialProject.vue'
+import BallCascadeProject from '../p5Projects/BallCascade/BallCascadeProject.vue'
+import JoyDivisionLinesProject from '../p5Projects/JoyDivisionLines/JoyDivisionLinesProject.vue'
 import GrowingCircleProject from '../p5Projects/GrowingCircle/GrowingCircleProject.vue'
 import WaterfallProject from '../p5Projects/Waterfall/WaterfallProject.vue'
-import PerlinFlowFieldProject from "../p5Projects/PerlinFlowField/PerlinFlowFieldProject.vue"
-import FireworksProject from "../p5Projects/Fireworks/FireworksProject.vue"
-import FourierSeriesProject from "../p5Projects/FourierSeries/FourierSeriesProject.vue"
-
-
+import PerlinFlowFieldProject from '../p5Projects/PerlinFlowField/PerlinFlowFieldProject.vue'
+import FireworksProject from '../p5Projects/Fireworks/FireworksProject.vue'
+import FourierSeriesProject from '../p5Projects/FourierSeries/FourierSeriesProject.vue'
 import { onMounted, onUnmounted, shallowRef } from 'vue'
 
-const projects = shallowRef([FourierSeriesProject, WaterfallProject, PerlinFlowFieldProject, GrowingCircleProject, FireworksProject, JoyDivisionLinesProject, RadialProject, BallCascadeProject]);
-
-
+const projects = shallowRef([
+    FourierSeriesProject,
+    WaterfallProject,
+    PerlinFlowFieldProject,
+    GrowingCircleProject,
+    FireworksProject,
+    JoyDivisionLinesProject,
+    RadialProject,
+    BallCascadeProject
+])
+// const projects = shallowRef([FourierSeriesProject, WaterfallProject])
+// const projects2 = shallowRef([PerlinFlowFieldProject, GrowingCircleProject])
 
 function preventDefaultForScrollKeys(event: KeyboardEvent) {
-    const keysToPrevent = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']; // Arrow keys
+    const keysToPrevent = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'] // Arrow keys
     if (keysToPrevent.includes(event.code)) {
-        event.preventDefault();
+        event.preventDefault()
     }
 }
 
 onMounted(() => {
-    window.addEventListener('keydown', e => {preventDefaultForScrollKeys(e)});
+    window.addEventListener('keydown', (e) => {
+        preventDefaultForScrollKeys(e)
+    })
 })
 
 onUnmounted(() => {
-    window.removeEventListener('keydown', e => {preventDefaultForScrollKeys(e)});
+    window.removeEventListener('keydown', (e) => {
+        preventDefaultForScrollKeys(e)
+    })
 })
-
 </script>
 
 <template>
-<div class="snap-y snap-mandatory h-screen overflow-scroll">
-  <component v-for="project in projects" 
-            v-bind:is="project"
-            v-bind:key="project.title">
-  </component>
-</div>  
+    <div class="snap-y snap-mandatory h-screen overflow-scroll">
+        <component v-for="project in projects" v-bind:is="project" v-bind:key="project.title">
+        </component>
+    </div>
 </template>
