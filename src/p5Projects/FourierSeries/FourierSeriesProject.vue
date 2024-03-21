62ed986c18a0afe817e978b0d1a8f268a451657c
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-21 15:00:58 +1300

    refactor: reformat with prettier with tabs at 4 spaces

--- src/p5Projects/FourierSeries/FourierSeriesProject.vue
+++ src/p5Projects/FourierSeries/FourierSeriesProject.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-import P5Project from "../../components/P5Project.vue";
-import script from "./script"
-
+import P5Project from '../../components/P5Project.vue'
+import script from './script'
 </script>
-
 
 <template>
     <P5Project
-      title="Fourier Series"
-      scriptID="fourier-series"
-      description="spin"
-      usageInstructions="Approximating a square wave with a sum of trigonometric functions. Press the up and down arrows"
-      :script="script"
-      :maxCanvasDimensions="{
-        width: 400,
-        height: 500
-      }"
+        title="Fourier Series"
+        scriptID="fourier-series"
+        description="spin"
+        usageInstructions="Approximating a square wave with a sum of trigonometric functions. Press the up and down arrow2s"
+        :script="script"
+        :maxCanvasDimensions="{
+            width: 400,
+            height: 500
+        }"
     />
 </template>
