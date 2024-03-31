fe5c516bb7fabdf1a9ba9df4d5118eaf674896bc
Scott Hitchcock <scotthitch5@gmail.com>
2024-03-31 15:12:52 +1300

    feat: set Inter font as default

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,12 @@ import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <NavBar />
-
-  <!-- <div class="fixed hidden sm:block left-[31%] w-0.5 bg-black" style="top: 3%; bottom: 3%;"></div> -->
-
-  <!-- <div class="absolute sm:left-[31%] w-screen sm:w-[69%] sm:max-w-[40rem] px-3 sm:px-4"> -->
+    <NavBar />
     <RouterView />
-  <!-- </div> -->
 </template>
 
 <style scoped>
-
-/* @import url(
-  'https://fonts.googleapis.com/css2?family=Passion+One:wght@400;700;900&display=swap'); 
-
 html * {
-   font-family: 'Passion One' !important;
-} */
-
+    font-family: 'Inter' !important;
+}
 </style>
