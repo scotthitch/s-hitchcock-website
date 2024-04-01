cbae677e6a638f805dadb60bc51b4d464315e890
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-02 10:41:36 +1300

    feat: create new home page concept

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,48 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="fade-in">
+    <!-- <div class="fade-in">
         <div
-            class="w-100svw pt-6 sm:pt-12 text-black text-center text-6xl sm:text-7xl font-bold slide-from-right"
+            class="w-100svw pt-6 sm:pt-12 text-6lack text-6enter text-6xl sm:text-6xl sm:text-8xl slide-from-left-40 font-bold italic slide-from-left-40 slide-from-right-40
+            "
         >
             Scott Hitchcock
         </div>
         <div
-            class="w-100svw pt-2 text-gray-700 text-center text-xl sm:text-2xl font-bold italic slide-from-left"
+            class="w-100svw pt-2 text-6ray-400 text-6enter text-6l sm:text-8xl font-bold italic italic slide-from-left-40"
         >
             Mechatronics Engineer
         </div>
 
         <div
-            class="w-100svw pt-2 text-gray-700 text-center text-xl sm:text-2xl font-bold italic slide-from-left"
+            class="w-100svw pt-2 text-6ray-400 text-6enter text-6l sm:text-8xl font-bold italic italic slide-from-left-40"
         >
             Welcome to my home page
+        </div>
+    </div> -->
+    <div class="flex h-screen">
+        <div class="m-auto grid grid-cols-3 fade-in">
+            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-40">Hi,</div>
+            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-40">I'm</div>
+            <div
+                class="flex flex-col-reverse text-6xs sm:text-2xl text-neutral-500 italic font-semibold slide-from-right-40"
+            >
+                <div>Engineer</div>
+                <div>Mechatronics</div>
+            </div>
+            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-60 col-span-2">
+                Scott
+            </div>
+
+            <div
+                class="flex flex-col-reverse text-6xs sm:text-2xl text-neutral-500 italic font-semibold slide-from-right-60"
+            >
+                <div>Developer</div>
+                <div>Software</div>
+            </div>
+            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-80 col-span-3">
+                Hitchcock
+            </div>
         </div>
     </div>
 </template>
