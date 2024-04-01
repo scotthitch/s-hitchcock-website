2c1c715a8df950446f86fa61c60a4e188c029eb8
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-01 20:40:44 +1300

    feat: create a scroll down indicator for projects view

---
+++ src/components/ScrollDownIndicator.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="relative top-[92vh] w-[100vw] z-30 flex flex-row justify-center items-center">
+        <div class="dot animate-bounce flex flex-col justify-center items-center">
+            <img src="../assets/arrow.svg" alt="arrow-icon" class="pl-2 w-[100%] h-[100%] rotate-90 scale-[0.6]" />
+        </div>
+    </div>
+</template>
+
+<style>
+.dot {
+    height: 48px;
+    width: 48px;
+    background-color: rgba(0, 0, 0, 0.35);
+    border-radius: 50%;
+    display: inline-block;
+}
+</style>
