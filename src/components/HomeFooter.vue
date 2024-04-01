181247f282883fb654421c32ef38c5a49a54b736
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-02 12:43:37 +1300

    feat: add footer for home page

---
+++ src/components/HomeFooter.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import HomeFooterIcon from './HomeFooterIcon.vue'
+</script>
+
+<template>
+    <div class="flex flex-col gap-4 items-center bottom-0 pb-2">
+        <div class="text-sm sm:text-base font-bold">Built With</div>
+
+        <div class="w-full flex flex-row gap-[12svw] sm:gap-28 md:gap-32 justify-center">
+            <HomeFooterIcon
+                file-name="vuejs.png"
+                icon-name="Vue.js"
+                icon-alt="vuejs"
+                external-link="https://vuejs.org/"
+            />
+            <HomeFooterIcon
+                file-name="tailwind.png"
+                icon-name="Tailwind CSS"
+                icon-alt="tailwind-css"
+                external-link="https://tailwindcss.com/"
+            />
+            <HomeFooterIcon
+                file-name="p5js.png"
+                icon-name="p5.js"
+                icon-alt="p5js"
+                external-link="https://p5js.org/"
+            />
+            <HomeFooterIcon
+                file-name="aws.png"
+                icon-name="AWS"
+                icon-alt="aws"
+                external-link="https://aws.amazon.com/"
+            />
+        </div>
+    </div>
+</template>
+
+<style></style>
---
+++ src/components/HomeFooterIcon.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import getImageURL from '../helpers/getImageURL'
+
+const props = defineProps<{
+    iconName: string
+    fileName: string
+    iconAlt: string
+    externalLink: string
+}>()
+</script>
+
+<template>
+    <a :href="props.externalLink">
+        <div class="flex flex-col gap-1 items-center">
+            <img
+                :src="getImageURL('buildToolsLogos/' + props.fileName)"
+                :alt="props.iconAlt"
+                class="h-7 sm:h-9"
+            />
+            <div class="text-neutral-700 text-sm">
+                {{ props.iconName }}
+            </div>
+        </div>
+    </a>
+</template>
+
+<style></style>
