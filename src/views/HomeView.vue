ca0f18bf27a4755d8529018d2bff3d4fea727177
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-02 13:05:23 +1300

    feat: make home screen less messy

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,9 @@ import HomeFooter from '../components/HomeFooter.vue'
 <template>
     <div class="flex h-screen">
         <div class="m-auto grid grid-cols-3">
-            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-40">Hi,</div>
-            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-40">I'm</div>
+            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-60 col-span-2">
+                Scott
+            </div>
 
             <div
                 class="flex flex-col-reverse text-sm sm:text-2xl text-neutral-500 italic font-semibold slide-from-right-40 text-end"
@@ -15,19 +16,8 @@ import HomeFooter from '../components/HomeFooter.vue'
                 <div>Mechatronics</div>
             </div>
 
-            <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-60 col-span-2">
-                Scott
-            </div>
-
-            <div
-                class="flex flex-col-reverse text-sm sm:text-2xl text-neutral-500 italic font-semibold slide-from-right-60 text-end"
-            >
-                <div>Developer</div>
-                <div>Software</div>
-            </div>
-
             <div class="text-6xl sm:text-8xl font-bold italic slide-from-left-80 col-span-3">
-                Hitchcock.
+                Hitchcock
             </div>
         </div>
     </div>
