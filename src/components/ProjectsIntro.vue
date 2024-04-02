c9f671a716cfaab6d3fd425f3725754bc873c4e0
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-03 00:17:50 +1300

    fix: change h-screen to 100svh for ios mobile

--- src/components/ProjectsIntro.vue
+++ src/components/ProjectsIntro.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="verticalPan relative h-[94svh] sm:h-[92svh] snap-always snap-center bg-cv-blue">
-        <div class="flex h-screen">
+    <div class="verticalPan relative h-[92svh] sm:h-[92svh] snap-always snap-center bg-cv-blue">
+        <div class="flex h-[100svh]">
             <div class="m-auto text-6xl sm:text-8xl font-bold italic slide-from-left-60 text-white">
                 Projects
             </div>
