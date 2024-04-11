9dc13c29d6c91a02a8251f7b2ee375d4515e6fe0
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-12 10:01:43 +1200

    fix: keep navbar at top of screen for resume view

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -4,7 +4,7 @@ import cvURL from '../assets/scott_hitchcock_cv.pdf';
 </script>
 
 <template>
-  <div class="flex flex-col items-center p-2 sm:py-6">
+  <div class="h-full overflow-scroll flex flex-col items-center p-2 sm:py-6">
     <div class="flex flex-col gap-2 sm:w-1/2 sm:min-w-[600px] max-w-[700px]">
       <img src="../assets/scott_hitchcock_cv-1.png" class="mx-auto shadow-md">
       <img src="../assets/scott_hitchcock_cv-2.png" class="mx-auto shadow-md">
