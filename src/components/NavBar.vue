797eb305a184bcd59a4ea8c3938456860d9c1692
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-15 08:13:22 +1200

    feat: make navbar larger

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -21,14 +21,14 @@ const navBarRoutes = [
 <template>
     <!-- Add some negative margin on the bottom to ensure navbar overlaps whatevever's visible below -->
     <nav class="basis-12 z-30 items-center bg-col-primary -mb-[0.5px]" id="navbar">
-        <div class="flex flex-row w-full gap-6 py-4 items-center">
-            <div class="italic text-col-secondary text-xl xs:text-2xl font-bold pl-6">
+        <div class="flex flex-row w-full gap-6 py-5 items-center">
+            <div class="italic text-col-secondary text-2xl xs:text-3xl font-bold pl-6">
                 <div class="hidden sm:block pr-10">Scott Hitchcock</div>
                 <div class="block sm:hidden pr-6">S.H</div>
             </div>
             <div v-for="(navBarRoute, index) in navBarRoutes" v-bind:key="index">
                 <div
-                    class="text-col-secondary hover:scale-110 text-base xs:text-lg navbarRoute font-semibold"
+                    class="text-col-secondary hover:scale-110 text-lg xs:text-xl navbarRoute font-semibold"
                 >
                     <router-link :to="navBarRoute.to" :id="navBarRoute.id">{{
                         navBarRoute.title
