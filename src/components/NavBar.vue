45690566f1b99a7053ff2b276f90892f634ddc6c
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-15 09:15:23 +1200

    feat: update navbar styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -20,20 +20,26 @@ const navBarRoutes = [
 
 <template>
     <!-- Add some negative margin on the bottom to ensure navbar overlaps whatevever's visible below -->
-    <nav class="basis-12 z-30 items-center bg-col-primary -mb-[0.5px]" id="navbar">
-        <div class="flex flex-row w-full gap-6 py-5 items-center">
-            <div class="italic text-col-secondary text-2xl xs:text-3xl font-bold pl-6">
+    <nav
+        class="basis-12 z-30 items-center bg-col-primary -mb-[0.5px] flex flex-row w-full gap-6 py-6"
+        id="navbar"
+    >
+        <div class="italic text-col-secondary text-xl xs:text-2xl font-semibold pl-6" id="navbarTitle">
+            <router-link to="/">
                 <div class="hidden sm:block pr-10">Scott Hitchcock</div>
                 <div class="block sm:hidden pr-6">S.H</div>
-            </div>
-            <div v-for="(navBarRoute, index) in navBarRoutes" v-bind:key="index">
-                <div
-                    class="text-col-secondary hover:scale-110 text-lg xs:text-xl navbarRoute font-semibold"
+            </router-link>
+        </div>
+        <div v-for="(navBarRoute, index) in navBarRoutes" v-bind:key="index">
+            <div
+                class="text-neutral-600 hover:text-neutral-200 text-sm xs:text-base navbarRoute font-semibold"
+            >
+                <router-link
+                    :to="navBarRoute.to"
+                    :id="navBarRoute.id"
+                    exact-active-class="text-neutral-200"
+                    >{{ navBarRoute.title }}</router-link
                 >
-                    <router-link :to="navBarRoute.to" :id="navBarRoute.id">{{
-                        navBarRoute.title
-                    }}</router-link>
-                </div>
             </div>
         </div>
     </nav>
@@ -41,25 +47,6 @@ const navBarRoutes = [
 
 <style>
 .navbarRoute {
-    transition: 0.15s ease-in-out;
-}
-
-.nav-bar-background {
-    transition: 0.55s ease-in-out;
-}
-
-.arrow-enter-active,
-.arrow-leave-active {
-    transition: 0.45s ease;
-}
-
-.arrow-enter-from,
-.arrow-leave-to {
-    opacity: 0;
-    transform: scale(0);
-}
-
-.router-link-exact-active {
-    text-decoration: underline;
+    transition: 0.25s ease-in-out;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,27 +3,27 @@ import HomeView from '../views/HomeView/HomeView.vue'
 import ResumeView from '../views/ResumeView/ResumeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/resume',
-      name: 'resume',
-      component: ResumeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      // route level code-splitting
-      // this generates a separate chunk for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProjectsView/ProjectsView.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/resume',
+            name: 'resume',
+            component: ResumeView
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            // route level code-splitting
+            // this generates a separate chunk for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/ProjectsView/ProjectsView.vue')
+        }
+    ]
 })
 
 export default router
