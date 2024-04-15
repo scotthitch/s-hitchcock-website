e0936724ad1e3e39d4724b6fd1d4fbfdb33f9b1b
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-16 11:04:18 +1200

    feat: remove gh icon from footer

--- src/views/HomeView/HomeFooter.vue
+++ src/views/HomeView/HomeFooter.vue
@@ -6,7 +6,7 @@ import HomeFooterIcon from './HomeFooterIcon.vue'
     <div class="relative flex flex-col gap-2 items-center bottom-0 w-full pb-4 pt-2 bg-col-light">
         <div class="text-sm sm:text-base font-bold text-col-dark">Tools Used</div>
 
-        <div class="w-full flex flex-row gap-[8svw] sm:gap-20 md:gap-24 justify-center">
+        <div class="w-full flex flex-row gap-[12svw] sm:gap-20 md:gap-24 justify-center">
             <HomeFooterIcon
                 file-name="vuejs.png"
                 icon-name="Vue.js"
@@ -25,12 +25,12 @@ import HomeFooterIcon from './HomeFooterIcon.vue'
                 icon-alt="p5js"
                 external-link="https://p5js.org/"
             />
-            <HomeFooterIcon
+            <!-- <HomeFooterIcon
                 file-name="github.png"
                 icon-name="GitHub"
                 icon-alt="github"
                 external-link="https://github.com/scotthitch/s-hitchcock-website/"
-            />
+            /> -->
             <HomeFooterIcon
                 file-name="aws.png"
                 icon-name="AWS"
