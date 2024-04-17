ad2cebc2178810ba8eaed02de226b0d1cd43a206
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-18 09:57:16 +1200

    refactor: put the form into its own component

---
+++ src/views/ContactMeView/ContactMeForm.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const HERO_TOFU_CONTACT_ME_URL = import.meta.env.VITE_HERO_TOFU_CONTACT_ME_URL
+</script>
+
+<template>
+    <form
+        class="my-32"
+        :action="HERO_TOFU_CONTACT_ME_URL"
+        method="post"
+        id="contact-form"
+        accept-charset="UTF-8"
+    >
+    
+        <div>
+            <label for="name">Your Name</label>
+            <input name="Name" id="name" type="text" required />
+        </div>
+        <div>
+            <label for="email">Your Email</label>
+            <input name="Email" id="email" type="email" required />
+        </div>
+        <div>
+            <label for="content">Content</label>
+            <input name="Content" id="content" type="text" required />
+        </div>
+        <div>
+            <button type="submit" form="contact-form" value="Submit">Submit</button>
+
+            <div
+                style="
+                    text-indent: -99999px;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    position: absolute;
+                "
+                aria-hidden="true"
+            >
+                <input type="text" name="_gotcha" tabindex="-1" autocomplete="off" />
+            </div>
+        </div>
+    </form>
+</template>
--- src/views/ContactMeView/ContactMeView.vue
+++ src/views/ContactMeView/ContactMeView.vue
@@ -1,39 +1,16 @@
 <script setup lang="ts">
-
 import PageIntro from '../../components/PageIntro.vue'
+import ContactMeForm from './ContactMeForm.vue'
 
-const HERO_TOFU_CONTACT_ME_URL = import.meta.env.VITE_HERO_TOFU_CONTACT_ME_URL
 </script>
 
 <template>
-    <PageIntro pageName="Contact Me" />
+    <div class="overflow-scroll">
+        <PageIntro pageName="Contact Me" />
+        <div class="m-auto grid grid-cols-3">
+            <div>Thank you for visiting</div>
 
-    <form :action="HERO_TOFU_CONTACT_ME_URL" method="post" accept-charset="UTF-8">
-        <div>
-            <label for="name">Your Name</label>
-            <input name="Name" id="name" type="text" required />
+            <ContactMeForm />
         </div>
-        <div>
-            <label for="email">Your Email</label>
-            <input name="Email" id="email" type="email" required />
-        </div>
-        <div>
-            <label for="content">Content</label>
-            <input name="Content" id="content" type="text" required />
-        </div>
-        <div>
-            <input type="submit" value="Download CTA" />
-            <div
-                style="
-                    text-indent: -99999px;
-                    white-space: nowrap;
-                    overflow: hidden;
-                    position: absolute;
-                "
-                aria-hidden="true"
-            >
-                <input type="text" name="_gotcha" tabindex="-1" autocomplete="off" />
-            </div>
-        </div>
-    </form>
+    </div>
 </template>
