02a444ac2e662232d8c01e08203185a5182870e4
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-17 12:04:41 +1200

    refactor: clean up old ball cascade script

--- src/views/ProjectsView/p5Projects/BallCascade/Ball.ts
+++ src/views/ProjectsView/p5Projects/BallCascade/Ball.ts
@@ -1,45 +1,50 @@
 import P5 from 'p5'
+import selectRandomElementFromArray from '@/helpers/selectRandomElementFromArray'
+
+const COLOURS = ['#ffbe0b', '#fb5607', '#ff006e', '#8338ec', '#3a86ff']
+const MIN_BALL_RADIUS = 5
+const MAX_BALL_RADIUS = 10
+const INITIAL_VELOCITY_X = 1.1
+const INITIAL_VELOCITY_Y = 2
 
 class Ball {
     p5Instance: P5
     r: number
-    xPos: number
-    yPos: number
-    xVel: number
-    yVel: number
-    yAcc: number
-    colours: string[]
-    index: number
+
+    position: P5.Vector
+    velocity: P5.Vector
+    acceleration: P5.Vector
     colour: string
 
-    constructor(p5Instance: P5, xPos: number, yPos: number, gravDir: number) {
+    constructor(p5Instance: P5, position: P5.Vector, gravity: number) {
         this.p5Instance = p5Instance
-        this.r = p5Instance.random(5, 10)
-        this.xPos = xPos
-        this.yPos = yPos
-        this.xVel = p5Instance.random(-1.1, 1.1)
-        if (gravDir > 0) {
-            this.yVel = p5Instance.random(0, -2)
-        } else {
-            this.yVel = p5Instance.random(0, 1)
-        }
-        this.yAcc = 0.1 * gravDir
+        this.r = p5Instance.random(MIN_BALL_RADIUS, MAX_BALL_RADIUS)
 
-        this.colours = ['#ffbe0b', '#fb5607', '#ff006e', '#8338ec', '#3a86ff']
-        this.index = p5Instance.floor(p5Instance.random(0, this.colours.length))
-        this.colour = this.colours[this.index]
+        // Set position
+        this.position = position
+
+        // Set initial velocity
+        this.velocity = p5Instance.createVector(
+            p5Instance.random(-INITIAL_VELOCITY_X, INITIAL_VELOCITY_X),
+            p5Instance.random(-INITIAL_VELOCITY_Y, INITIAL_VELOCITY_Y)
+        )
+
+        // Set acceleration only in y direction (assume unit mass)
+        this.acceleration = p5Instance.createVector(0, gravity)
+
+        // Set a random ball colour
+        this.colour = selectRandomElementFromArray(COLOURS)
     }
 
     move() {
-        this.yVel += this.yAcc
-        this.xPos += this.xVel
-        this.yPos += this.yVel
+        this.velocity.add(this.acceleration)
+        this.position.add(this.velocity)
     }
 
     render() {
         this.p5Instance.noStroke()
         this.p5Instance.fill(this.colour)
-        this.p5Instance.ellipse(this.xPos, this.yPos, this.r)
+        this.p5Instance.ellipse(this.position.x, this.position.y, this.r)
     }
 }
 
--- src/views/ProjectsView/p5Projects/BallCascade/script.ts
+++ src/views/ProjectsView/p5Projects/BallCascade/script.ts
@@ -5,32 +5,39 @@ import Ball from './Ball'
 const scriptWrapper: p5ScriptWrapper = (screenDimensions: ScreenDimensions): p5Script => {
     const script = (p5Instance: P5): void => {
         const BACKGROUND_COLOUR = 0
+        const MAX_NUMBER_BALLS = 250
         const balls: Ball[] = []
-        let gravDir = 1
+        let gravity = 0.1
 
         // Setup the canvas
         p5Instance.setup = () => {
             p5Instance.createCanvas(screenDimensions.width, screenDimensions.height)
         }
 
-        // Redraw the canvas on every iteration
+        // Redraw the canvas on every iterations
         p5Instance.draw = () => {
             p5Instance.background(BACKGROUND_COLOUR)
-            if (!(p5Instance.mouseX == 0 && p5Instance.mouseY == 0)) {
-                balls.push(new Ball(p5Instance, p5Instance.mouseX, p5Instance.mouseY, gravDir))
-                for (const ball of balls) {
-                    ball.move()
-                    ball.render()
-                }
 
-                if (balls.length > 100) {
-                    balls.splice(0, 1)
-                }
+            // If the mouse is at (0,0) this indicates the sketch likely hasn't been interacted with yet
+            if (p5Instance.mouseX == 0 && p5Instance.mouseY == 0) {
+                // So do nothing and return
+                return
+            }
+
+            const mousePosition = p5Instance.createVector(p5Instance.mouseX, p5Instance.mouseY)
+            balls.push(new Ball(p5Instance, mousePosition, gravity))
+            for (const ball of balls) {
+                ball.move()
+                ball.render()
+            }
+
+            if (balls.length > MAX_NUMBER_BALLS) {
+                balls.splice(0, 1)
             }
         }
 
         p5Instance.mousePressed = () => {
-            gravDir *= -1
+            gravity *= -1
         }
     }
     return script
