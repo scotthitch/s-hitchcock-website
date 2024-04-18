bde3972e26f41c5ed423b2219ae84b7ce443a624
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-19 09:41:09 +1200

    refactor: remove all project components files and now render all projects directly in projectsview

--- src/views/ProjectsView/projects.ts
+++ src/views/ProjectsView/projects.ts
@@ -1,4 +1,13 @@
 import ballCascadeScriptWrapper from './p5Projects/BallCascade'
+import fireworksScriptWrapper from './p5Projects/Fireworks'
+import fourierSeriesScriptWrapper from './p5Projects/FourierSeries'
+import growingCircleScriptWrapper from './p5Projects/GrowingCircle'
+import perlinFlowFieldScriptWrapper from './p5Projects/PerlinFlowField'
+import radialScriptWrapper from './p5Projects/Radial'
+import tilingScriptWrapper from './p5Projects/Tiling'
+import unknownPleasuresScriptWrapper from './p5Projects/UnknownPleasures'
+import waterfallScriptWrapper from './p5Projects/Waterfall'
+
 import type { p5ScriptWrapper } from '@/types'
 
 interface projectsType {
@@ -11,10 +20,68 @@ interface projectsType {
 
 const projects: projectsType[] = [
     {
+        title: 'Waterfall',
+        scriptID: 'waterfall',
+        description: 'A cascade of veils built using Perlin Noise',
+        scriptWrapper: waterfallScriptWrapper,
+        isMobileOrTabletFriendly: true
+    },
+    {
+        title: 'Fourier Series',
+        scriptID: 'fourier-series',
+        description:
+            'Sinusoidal terms summing to approximate a square wave. Click to change the number of terms',
+        scriptWrapper: fourierSeriesScriptWrapper,
+        isMobileOrTabletFriendly: true
+    },
+    {
+        title: 'Unknown Pleasures',
+        scriptID: 'unknown-pleasures',
+        description:
+            "Inspired by Joy Division's Unknown Pleasures album art and built using Gaussian distribution plus some random noise",
+        scriptWrapper: unknownPleasuresScriptWrapper,
+        isMobileOrTabletFriendly: false
+    },
+    {
+        title: 'Perlin Flow Field',
+        scriptID: 'perlin-flow-field',
+        description: 'A random vector flow field generated using 2D Perlin Noise.',
+        scriptWrapper: perlinFlowFieldScriptWrapper,
+        isMobileOrTabletFriendly: true
+    },
+    {
+        title: 'Tiling',
+        scriptID: 'tiling',
+        description: 'Click to change the zoom and tilt',
+        scriptWrapper: tilingScriptWrapper,
+        isMobileOrTabletFriendly: true
+    },
+    {
+        title: 'Radial',
+        scriptID: 'radial',
+        description: 'Press the up and down arrows to change the number of reflections',
+        scriptWrapper: radialScriptWrapper,
+        isMobileOrTabletFriendly: false
+    },
+    {
         title: 'Ball Cascade',
         scriptID: 'ball-cascade',
         description: 'Not much to do here. You can flip gravity by clicking',
         scriptWrapper: ballCascadeScriptWrapper,
+        isMobileOrTabletFriendly: false
+    },
+    {
+        title: 'Growing Circle',
+        scriptID: 'growing-circle',
+        description: 'Just sit back and relax',
+        scriptWrapper: growingCircleScriptWrapper,
+        isMobileOrTabletFriendly: true
+    },
+    {
+        title: 'Fireworks',
+        scriptID: 'fireworks',
+        description: 'Click to shoot some fireworks.',
+        scriptWrapper: fireworksScriptWrapper,
         isMobileOrTabletFriendly: true
     }
 ]
