ced8574af0ddf19c2c21f9b87c682ef9976aa423
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-19 10:06:41 +1200

    feat: add some padding around navbar buttons

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -30,19 +30,20 @@ const navBarRoutes = [
         id="navbar"
     >
         <div class="italic text-col-light text-xl xs:text-2xl font-semibold" id="navbarTitle">
-            <router-link to="/">
-                <div class="hidden sm:block mr-10">Scott Hitchcock</div>
-                <div class="block sm:hidden mr-2">S.H</div>
+            <router-link to="/" class="py-2">
+                <div class="hidden sm:block">Scott Hitchcock</div>
+                <div class="block sm:hidden">S.H</div>
             </router-link>
         </div>
 
-        <div class="flex flex-row gap-4 sm:gap-8">
+        <div class="flex flex-row gap-3 xs:gap-4 md:gap-6">
             <div
                 class="text-col-mid hover:text-col-light text-sm xs:text-base navbarRoute font-semibold"
                 v-for="(navBarRoute, index) in navBarRoutes"
                 v-bind:key="index"
             >
                 <router-link
+                    class="sm:p-2"
                     :to="navBarRoute.to"
                     :id="navBarRoute.id"
                     exact-active-class="text-col-light"
