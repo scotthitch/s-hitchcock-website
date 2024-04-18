2cf1eb3f66f60c9aa714ac86cd075769af2616dc
Scott Hitchcock <scotthitch5@gmail.com>
2024-04-18 22:21:45 +1200

    feat: move nav links to right side of navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -26,7 +26,7 @@ const navBarRoutes = [
 <template>
     <!-- Add some negative margin on the bottom to ensure navbar overlaps whatevever's visible below -->
     <nav
-        class="basis-12 z-30 items-center bg-col-dark -mb-[0.5px] flex flex-row justify-between sm:justify-normal w-full sm:gap-8 p-6"
+        class="basis-12 z-30 items-center bg-col-dark -mb-[0.5px] flex flex-row justify-between w-full px-6 sm:px-10 py-6"
         id="navbar"
     >
         <div class="italic text-col-light text-xl xs:text-2xl font-semibold" id="navbarTitle">
@@ -36,17 +36,19 @@ const navBarRoutes = [
             </router-link>
         </div>
 
-        <div
-            class="text-col-mid hover:text-col-light text-sm xs:text-base navbarRoute font-semibold"
-            v-for="(navBarRoute, index) in navBarRoutes"
-            v-bind:key="index"
-        >
-            <router-link
-                :to="navBarRoute.to"
-                :id="navBarRoute.id"
-                exact-active-class="text-col-light"
-                >{{ navBarRoute.title }}</router-link
+        <div class="flex flex-row gap-4 sm:gap-8">
+            <div
+                class="text-col-mid hover:text-col-light text-sm xs:text-base navbarRoute font-semibold"
+                v-for="(navBarRoute, index) in navBarRoutes"
+                v-bind:key="index"
             >
+                <router-link
+                    :to="navBarRoute.to"
+                    :id="navBarRoute.id"
+                    exact-active-class="text-col-light"
+                    >{{ navBarRoute.title }}</router-link
+                >
+            </div>
         </div>
     </nav>
 </template>
