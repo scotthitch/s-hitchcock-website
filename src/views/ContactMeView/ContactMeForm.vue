707d347e9c95e8402a86fe2258aff3a1106600d1
Scott Hitchcock <scotthitch5@gmail.com>
2024-05-03 11:15:51 +1200

    feat: contact me form more responsive

--- src/views/ContactMeView/ContactMeForm.vue
+++ src/views/ContactMeView/ContactMeForm.vue
@@ -7,10 +7,10 @@ const HERO_TOFU_CONTACT_ME_URL = import.meta.env.VITE_HERO_TOFU_CONTACT_ME_URL
 </script>
 
 <template>
-    <div class="p-8 lg:p-16 content-center">
+    <div class="p-6 sm:p-8 lg:p-16 content-center">
         <div class="font-bold italic text-4xl md:text-5xl">Thank you for visiting.</div>
 
-        <div class="font-semibold text-lg pt-6 pb-8">
+        <div class="font-semibold text-lg py-2 sm:pt-6 sm:pb-8">
             Please fill out the form to get in touch with me
         </div>
 
