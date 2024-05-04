8ab003c4a6984f305f3da3bb43881b020d9c0b13
Scott Hitchcock <scotthitch5@gmail.com>
2024-05-04 15:26:58 +1200

    turn off ssr for projects
    i believe that p5 projects cant be server side rendered

--- nuxt-s-hitchcock-website/nuxt.config.ts
+++ nuxt-s-hitchcock-website/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    routeRules: {
+        "/projects": { ssr: false },
+      },
     devtools: { enabled: true },
     modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@pinia/nuxt'],
     css: ['~/assets/css/main.css']
