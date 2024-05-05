88c3423548b9d1c6a35df857f01724a2abf17a55
Scott Hitchcock <scotthitch5@gmail.com>
2024-05-05 18:29:12 +1200

    disable ssr

--- nuxt-s-hitchcock-website/nuxt.config.ts
+++ nuxt-s-hitchcock-website/nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    routeRules: {
-        "/projects": { ssr: false },
-        "/contact": { ssr: false },
-      },
+  ssr: false,
+    // routeRules: {
+    //     "/projects": { ssr: false },
+    //     "/contact": { ssr: false },
+    //   },
     devtools: { enabled: true },
     modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@pinia/nuxt'],
     css: ['~/assets/css/main.css']
