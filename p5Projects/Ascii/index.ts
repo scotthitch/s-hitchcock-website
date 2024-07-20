37466424961d5c57b99e928cc6ef718a99c3b666
Scott Hitchcock <scotthitch5@gmail.com>
2024-07-21 10:31:13 +1200

    feat: dispalying image from 96x96 video feed

--- p5Projects/Ascii/index.ts
+++ p5Projects/Ascii/index.ts
@@ -1,34 +1,44 @@
 import P5 from 'p5' // Package from npm
 import type { p5Script, p5ScriptWrapper, ScreenDimensions } from '~/types'
 
+interface P5VideoElement extends P5.Element {
+    pixels: number[]
+    loadPixels: () => null
+    updatePixels: () => null
+}
+
 const scriptWrapper: p5ScriptWrapper = (screenDimensions: ScreenDimensions): p5Script => {
-    const myAscii = "@&%$#[]{}|()?/*!+<>;=^,_:'-.`"
-    console.log(myAscii.length)
-
     const script = (p5Instance: P5): void => {
-        let video: P5.Element
-
+        // const myAscii = "░░░@&%$#[]{}|()?/*!+>;=^,_:'-.`                       "
+        // const myAscii = '        .:░▒▓█'.split('').reverse().join('')
+        const myAscii = "░░░$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,^`'.                                         "
+        const brightnessToAscii = (brightnessValue: number): string => {
+            const index = Math.floor(p5Instance.map(brightnessValue, 0, 255, 0, myAscii.length - 1))
+            return myAscii[index]
+        }
+        let video: P5VideoElement
+        let asciiDiv: P5.Element
         // const img = p5Instance.loadImage('cam.jpg')
         // Setup the canvas
-        p5Instance.setup = () => {
-            let constraints = {
-                audio: false
-            }
 
+        const size = 96
+        p5Instance.setup = () => {
+            asciiDiv = p5Instance.createDiv()
+            asciiDiv.style(
+                "margin: 0; padding: 0; font-family: 'Courier'; font-size: 8pt; color: #0A100D; background-color: #DAFEB7; line-height: 6pt; display: block"
+            )
             p5Instance.createCanvas(screenDimensions.width, screenDimensions.height)
-            p5Instance.background(0)
-            video = p5Instance.createCapture('video')
+            video = p5Instance.createCapture('video', { flipped: true })
+            video.size(size*2, size)
             video.hide()
+            p5Instance.noCanvas()
 
             // p5Instance.image(img, 0, 0)
         }
         let counter = 0
         p5Instance.draw = () => {
-            p5Instance.background(0)
-
-            // p5Instance.translate(capture.width, 0)
-            // p5Instance.scale(-1, 1)
             video.loadPixels()
+            let asciiImage: string = ''
             for (let j = 0; j < video.height; j++) {
                 for (let i = 0; i < video.width; i++) {
                     let pixelIndex = (i + j * video.width) * 4
@@ -37,29 +47,18 @@ const scriptWrapper: p5ScriptWrapper = (screenDimensions: ScreenDimensions): p5S
                     let b = video.pixels[pixelIndex + 2]
 
                     let bright = 0.2126 * r + 0.7152 * g + 0.0722 * b
-                    // console.log(r, g, b)
-                    video.pixels[pixelIndex + 0] = bright
-                    video.pixels[pixelIndex + 1] = bright
-                    video.pixels[pixelIndex + 2] = bright
-                    // console.log('get')
-                    // let px = capture.get(x, y)
-                    // r = px
+
+                    let char = brightnessToAscii(bright)
+
+                    asciiImage += char == ' ' ? '&nbsp;' : char
+                    // asciiImage += char
                 }
+                asciiImage += '<br/>'
             }
-            video.updatePixels()
-            p5Instance.image(video, 0, 0)
-
-
-            //         let px = capture.get(x, y)
-            //         let r = px[0]
-            //         let g = px[1]
-            //         let b = px[2]
-            //         let bright = 0.2126 * r + 0.7152 * g + 0.0722 * b
-            //         capture.set(x, y, p5Instance.color(bright))
-            //     }
-            // }
-            // capture.updatePixels();
-            // p5Instance.image(capture, p5Instance.width/2,0);
+            asciiDiv.html(asciiImage)
+        }
+        p5Instance.mouseClicked = () => {
+            // p5Instance.noLoop()
         }
     }
     return script
