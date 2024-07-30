9e82ef980d5a531d8c67d3983322425529d41e11
Scott Hitchcock <scotthitch5@gmail.com>
2024-07-30 15:41:29 +1200

    update default ascii map

--- p5Projects/Ascii/pixelsToAscii.ts
+++ p5Projects/Ascii/pixelsToAscii.ts
@@ -2,7 +2,7 @@ import toGreyScale from './toGreyScale'
 import mapping from '../../helpers/mapping'
 
 const ASCII_ONLY_DENSITY_MAP =
-    "░░░$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft|()1{}[]?-_+~i!lI;:,^`'.".split('')
+    "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft|()1{}[]?-_+~i!lI;:,^`'.".split('')
 const N_SPACES = 35
 const ASCII_DENSITY_MAP = ASCII_ONLY_DENSITY_MAP.concat(Array(N_SPACES).fill(' '))
 
@@ -23,6 +23,7 @@ const pixelsToAscii = (
     for (let j = 0; j < imageHeight; j++) {
         // Iterate in reverse direction to horizontally flip the image
         for (let i = imageWidth - 1; i >= 0; i--) {
+        // for (let i = 0; i < imageWidth; i++) {
             // Get rgb values
             let pixelIndex = (i + j * imageWidth) * 4
             const r = pixels[pixelIndex + 0]
