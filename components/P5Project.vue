9cdec1f64c164bc2fd836914b06eba06fd2244ca
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-06 19:46:13 +1200

    feat: adjust where text sits on projects

--- components/P5Project.vue
+++ components/P5Project.vue
@@ -12,13 +12,13 @@ const props = defineProps<P5ProjectProps>()
                 <P5Canvas :scriptID="props.scriptID" :scriptWrapper="props.scriptWrapper" />
             </div>
 
-            <div class="absolute bottom-2 left-2 slide-from-left-40">
+            <div class="absolute bottom-2 left-4 slide-from-left-40">
                 <p class="text-5xl sm:text-6xl md:text-7xl font-bold italic text-white opacity-80">
                     {{ props.title }}
                 </p>
             </div>
 
-            <div class="absolute top-2 right-2 left-2 text-right fade-in slide-from-right-40">
+            <div class="absolute top-2 right-4 left-2 text-right fade-in slide-from-right-40">
                 <p class="text-base sm:text-lg lg:text-xl font-bold italic text-white opacity-80">
                     {{ props.description }}
                 </p>
