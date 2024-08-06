bb1ca80350e03f95c1b7c983e7469ab09ae2500b
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-07 10:17:18 +1200

    update contact form for mobile

--- pages/contact.vue
+++ pages/contact.vue
@@ -22,11 +22,14 @@ onUnmounted(() => {
 <template>
     <div class="h-full container lg:max-w-[1000px] mx-auto sm:px-12 content-center pb-12">
         <div
-            class="h-[90%] flex flex-col-reverse md:flex-row rounded-[32px] border-2 border-neutral-300 border-spacing-8 outline outline-2 outline-col-dark -outline-offset-[16px] sm:-outline-offset-[20px] bg-col-light"
+            class="h-[93%] flex flex-col-reverse md:flex-row rounded-[32px] border-2 border-neutral-300 border-spacing-8 outline outline-2 outline-col-dark -outline-offset-[24px] bg-col-light"
         >
-            <ContactMeForm class="basis-full md:basis-1/2" />
+            <ContactMeForm class="basis-full md:basis-1/2 m-4 md:m-2" />
             <div class="md:block md:basis-1/2 w-full h-[120px] md:h-auto">
-                <WavesCanvas :key="p5CanvasKey" class="rounded-r-[30px]" />
+                <WavesCanvas
+                    :key="p5CanvasKey"
+                    class="rounded-t-[30px] md:rounded-tl-[0px] md:rounded-br-[30px]"
+                />
             </div>
         </div>
     </div>
