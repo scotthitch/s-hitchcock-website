b33b8b673e8b895431a612e022b71ad46c8d3a2e
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-08 16:26:36 +1200

    feat: contact form more responsive for mobile

--- pages/contact.vue
+++ pages/contact.vue
@@ -19,12 +19,12 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="container mx-auto h-full content-center pb-12 sm:px-12 lg:max-w-[1000px]">
+    <div class="container mx-auto h-full content-center sm:px-12 sm:pb-12 lg:max-w-[1000px]">
         <div
             class="flex h-[93%] border-spacing-8 flex-col-reverse rounded-[32px] border-2 border-neutral-300 bg-col-light outline outline-2 -outline-offset-[24px] outline-col-dark md:flex-row"
         >
-            <ContactMeForm class="m-4 basis-full md:m-2 md:basis-1/2" />
-            <div class="h-[120px] w-full md:block md:h-auto md:basis-1/2">
+            <ContactMeForm class="mx-4 mb-4 basis-full md:m-2 md:basis-1/2" />
+            <div class="h-full w-full md:block md:basis-1/2">
                 <WavesCanvas
                     :key="p5CanvasKey"
                     class="rounded-t-[30px] md:rounded-br-[30px] md:rounded-tl-[0px]"
