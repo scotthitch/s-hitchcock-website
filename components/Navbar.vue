0ad1f5b89110ee8f7aa5c7b6945156229f6f2907
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-18 14:50:44 +1200

    feat: use pt-header

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -21,7 +21,7 @@ const navBarRoutes = [
 <template>
     <!-- Add some negative margin on the bottom to ensure navbar overlaps whatevever's visible below -->
     <header
-        class="fixed z-30 flex h-[var(--header-height)] w-full flex-row items-center justify-between bg-col-light px-6 sm:px-10"
+        class="h-header fixed z-30 flex w-full flex-row items-center justify-between bg-col-light px-6 sm:px-10"
         id="navbar"
     >
         <div class="text-xl font-semibold italic text-col-dark xs:text-2xl" id="navbarTitle">
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <Navbar />
     <!-- <main class="min-h-[calc(100vh-var(--header-height))]"> -->
 
-    <main class="pt-[var(--header-height)]">
+    <main class="pt-header">
         <div class="h-[calc(100.05svh-var(--header-height))]">
             <!-- <main> -->
             <slot />
