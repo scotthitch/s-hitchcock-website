8332b7cc95f48068f774dff393216ab8249efee8
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-20 20:22:17 +1200

    feat: fnc to check if small screen

--- helpers/deviceType.ts
+++ helpers/deviceType.ts
@@ -1,11 +1,15 @@
-const deviceType = navigator.userAgent
+const deviceType = navigator.userAgent;
 
-export const isMobileOrTablet = navigator.maxTouchPoints > 0 && !/Windows/i.test(deviceType)
+export const isMobileOrTablet = navigator.maxTouchPoints > 0 && !/Windows/i.test(deviceType);
 
-export const isDeviceAppleTouchScreen = /iPad|iPhone|iPod/.test(deviceType)
+export const isDeviceAppleTouchScreen = /iPad|iPhone|iPod/.test(deviceType);
 
 export const interactionEvent: 'touchend' | 'click' = isDeviceAppleTouchScreen
     ? 'touchend'
-    : 'click'
+    : 'click';
 
-export const isPortrait = screen.availHeight > screen.availWidth
+export const isPortrait = screen.availHeight > screen.availWidth;
+
+export const isSmallScreen = () => {
+    return window.innerWidth < 640;
+};
