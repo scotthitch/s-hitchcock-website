add8056195f2c0b3828825da7c3e69b477f80c1d
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-20 20:21:57 +1200

    feat: add in project modal component

---
+++ components/ProjectModal.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+// const emit = defineEmits(['closeModal']);
+import { defineEmits } from 'vue';
+const emit = defineEmits(['closeModal']);
+
+const closeModal = () => {
+    emit('closeModal');
+};
+
+const handleEscape = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+};
+
+onMounted(() => {
+    document.addEventListener('keydown', handleEscape);
+});
+
+onUnmounted(() => {
+    document.removeEventListener('keydown', handleEscape);
+});
+</script>
+
+<template>
+    <div
+        id="modal"
+        class="fixed inset-0 z-50 flex touch-none items-center justify-center bg-black bg-opacity-75 backdrop-blur-md"
+    >
+        <!-- Modal Content -->
+        <div class="relative mx-auto mt-12 h-[92%] w-[85%] py-4">
+            <button
+                @click="$emit('closeModal')"
+                id="close-modal"
+                class="absolute -right-4 -top-0 z-10 rounded-full bg-col-light p-3 text-col-dark"
+            >
+                <svg
+                    class="h-6 w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                    ></path>
+                </svg>
+            </button>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style></style>
