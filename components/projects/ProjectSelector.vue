a3de03125832d8b9fb00b93db4a6aac78326874b
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-25 15:40:28 +1200

    feat: use datefns with date attribute on project

--- components/projects/ProjectSelector.vue
+++ components/projects/ProjectSelector.vue
@@ -13,14 +13,14 @@ const handleClick = (i: number) => {
 
 <template>
     <div class="w-full px-6 pb-12 pt-8 md:w-1/3 md:px-8 lg:w-1/4 lg:px-10">
-        <ProjectsCheckThisOut class="text-col-bg block px-24 py-24 md:hidden" />
+        <ProjectsCheckThisOut class="block px-24 py-24 text-col-bg md:hidden" />
 
         <ul class="flex w-full flex-col">
             <li v-for="(project, i) in projects" :key="i" class="my-2 text-col-mid">
                 <button
                     @click="handleClick(i)"
                     :class="[
-                        'hover:bg-col-content hover:text-col-bg w-full px-4 py-3 text-center text-base font-medium duration-[20ms] ease-linear hover:scale-[1.02] hover:rounded-[25px] hover:bg-opacity-30 hover:shadow-lg hover:backdrop-blur-md md:text-left hover:md:scale-[1.05]',
+                        'w-full px-4 py-3 text-center text-base font-medium duration-[20ms] ease-linear hover:scale-[1.02] hover:rounded-[25px] hover:bg-col-content hover:bg-opacity-30 hover:text-col-bg hover:shadow-lg hover:backdrop-blur-md md:text-left hover:md:scale-[1.05]',
 
                         i == props.selectedProjectIndex && 'text-col-bg'
                     ]"
