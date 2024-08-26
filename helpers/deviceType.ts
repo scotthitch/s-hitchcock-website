7ec3d63908eb145cf8d514003194966991299498
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-27 11:23:23 +1200

    refactor: change isMobileOrTablet to a fnc

--- helpers/deviceType.ts
+++ helpers/deviceType.ts
@@ -1,6 +1,8 @@
 const deviceType = navigator.userAgent;
 
-export const isMobileOrTablet = navigator.maxTouchPoints > 0 && !/Windows/i.test(deviceType);
+export const isMobileOrTablet = () => {
+    return navigator.maxTouchPoints > 0 && !/Windows/i.test(deviceType);
+};
 
 export const isDeviceAppleTouchScreen = /iPad|iPhone|iPod/.test(deviceType);
 
