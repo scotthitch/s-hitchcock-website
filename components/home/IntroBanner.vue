9eac79e4d30fd6236572e5f754da326bdad57bc2
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-29 16:26:05 +1200

    feat: add intro banner componenet

---
+++ components/home/IntroBanner.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+    <div
+        class="container mx-auto -mt-24 flex h-[80svh] flex-col items-center justify-center lg:text-center"
+    >
+        <div class="text-6xl font-semibold italic text-col-bg sm:text-7xl">
+            Hey, I'm <span class="font-bold">Scott Hitchcock</span>
+        </div>
+        <div class="mt-4 text-2xl italic text-col-bg">
+            I’m a Mechatronics Engineer and Software Developer based in Ōtautahi, Christchurch
+        </div>
+    </div>
+</template>
+
+<style></style>
