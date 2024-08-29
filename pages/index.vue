9ed33a053f675d0e9bdca16b0dc997cb69842257
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-29 16:28:07 +1200

    feat: use links group

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="container mx-auto"></div>
+    <HomeLinksGroup />
+    <HomeIntroBanner />
     <HomeWorkExperienceViewer />
 </template>
 
