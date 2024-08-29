db173796d50cd833cb633b6c22f70a7458a97984
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-29 16:27:51 +1200

    feat: create a links group component for gh and linkedin componenets

---
+++ components/home/LinksGroup.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="mt-header-buffer absolute right-8 top-0 flex h-8 flex-row gap-3">
+        <SVGLinkedIn />
+        <SVGGitHub />
+    </div>
+</template>
+
+<style></style>
