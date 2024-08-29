953532cb1f310b10d8b18f8461f3c73e4db73b3b
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-30 10:33:08 +1200

    feat: adjust styling for intro banner

--- components/home/IntroBanner.vue
+++ components/home/IntroBanner.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts"></script>
 <template>
     <div
-        class="container mx-auto -mt-24 flex h-[80svh] flex-col items-center justify-center lg:text-center"
+        class="container mx-auto -mt-8 flex h-[80svh] flex-col justify-center text-start sm:-mt-24 sm:h-[83svh] lg:text-center"
     >
-        <div class="text-6xl font-semibold italic text-col-bg sm:text-7xl">
-            Hey, I'm <span class="font-bold">Scott Hitchcock</span>
+        <div class="text-7xl font-semibold italic text-col-bg sm:text-7xl">
+            Hey, <br class="md:hidden" />I'm Scott Hitchcock
         </div>
         <div class="mt-4 text-2xl italic text-col-bg">
             I’m a Mechatronics Engineer and Software Developer based in Ōtautahi, Christchurch
