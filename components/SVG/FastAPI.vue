119c6c4823c614c314db690104604e834fe1dcfc
Scott Hitchcock <scotthitch5@gmail.com>
2024-08-31 14:10:00 +1200

    feat: finish adding (almost) all svgs

---
+++ components/SVG/FastAPI.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+<template>
+    <svg
+        fill="none"
+        height="100%"
+        width="100%"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 154 154"
+    >
+        <circle cx="77" cy="77" fill="#05998b" r="77" />
+        <path d="M81.375 18.667l-38.75 70H77.5l-3.875 46.666 38.75-70H77.5z" fill="#fff" />
+    </svg>
+</template>
+
+<style></style>
