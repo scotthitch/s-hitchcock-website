febf411077826245a27b5c6a9f14935386810b3a
Scott Hitchcock <scotthitch5@gmail.com>
2024-09-05 17:27:14 +1200

    feat: add colour to see more button

--- components/home/SeeMore.vue
+++ components/home/SeeMore.vue
@@ -2,7 +2,7 @@
 
 <template>
     <button
-        class="bg-col-dark mx-auto mt-5 rounded-xl border-none bg-[#0137f5] px-6 py-4 text-col-content"
+        class="bg-col-dark mx-auto mt-5 rounded-xl border-none bg-[#29BAAD] px-6 py-4 text-col-content dark:text-col-bg"
     >
         <NuxtLink to="#work_experience">See More</NuxtLink>
     </button>
