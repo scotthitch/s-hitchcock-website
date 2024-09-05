596ab1f3bc5b3cbb177fa3ae133a4f5bbc06dc9c
Scott Hitchcock <scotthitch5@gmail.com>
2024-09-05 16:36:50 +1200

    feat: minor changes to intro banner

--- components/home/IntroBanner.vue
+++ components/home/IntroBanner.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts"></script>
 <template>
     <div
-        class="container mx-auto -mt-8 flex h-[80svh] flex-col justify-center text-start sm:-mt-24 sm:h-[83svh] lg:text-center"
+        class="container mx-auto -mt-[calc(var(--header-height)/2)] flex h-[100svh] flex-col justify-center text-start sm:-mt-header lg:text-center"
     >
         <div class="text-7xl font-semibold italic text-col-bg sm:text-7xl">
-            Hey, <br class="md:hidden" />I'm Scott Hitchcock
+            Kia Ora, <br class="md:hidden" />
+            I'm Scott Hitchcock
         </div>
-        <div class="mt-4 text-2xl italic text-col-bg">
+        <div class="mt-4 text-2xl italic text-col-mid">
             I’m a Mechatronics Engineer and Software Developer based in Ōtautahi, Christchurch
         </div>
     </div>
