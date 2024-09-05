35fc14bac9d5b78d25a06adbfd19da0e64ab1607
Scott Hitchcock <scotthitch5@gmail.com>
2024-09-05 17:15:57 +1200

    feat: button to scroll to work experience viwer

---
+++ components/home/SeeMore.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <button
+        class="bg-col-dark mx-auto mt-5 rounded-xl border-none bg-[#0137f5] px-6 py-4 text-col-content"
+    >
+        <NuxtLink to="#work_experience">See More</NuxtLink>
+    </button>
+</template>
+
+<style></style>
