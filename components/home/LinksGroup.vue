000ba697086c1de156ba5f224b0c9fda6be3efd5
Scott Hitchcock <scotthitch5@gmail.com>
2024-09-05 16:36:16 +1200

    feat: links group now top right and get's own colour on hover

--- components/home/LinksGroup.vue
+++ components/home/LinksGroup.vue
@@ -4,17 +4,31 @@ import { SVGGitHub, SVGLinkedIn } from '#components';
 const svgs: {
     component: Component;
     to: string;
+    hoverColour: string;
 }[] = [
-    { component: SVGLinkedIn, to: 'https://www.linkedin.com/in/scott-hitchcock/' },
+    {
+        component: SVGLinkedIn,
+        to: 'https://www.linkedin.com/in/scott-hitchcock/',
+        hoverColour: 'hover:fill-[#1F66C2]'
+    },
     {
         component: SVGGitHub,
-        to: 'https://github.com/scotthitch/'
+        to: 'https://github.com/scotthitch/',
+        hoverColour: 'hover:fill-[#010408]'
     }
 ];
 </script>
 <template>
-    <div class="absolute right-6 top-6 mt-header flex flex-row gap-3 sm:right-8">
-        <NuxtLink v-for="svg in svgs" :to="svg.to" class="size-7 sm:size-9">
+    <div
+        class="absolute right-12 top-6 mt-header flex flex-row items-center justify-center gap-3 text-center"
+    >
+        <NuxtLink
+            v-for="svg in svgs"
+            :to="svg.to"
+            class="size-7 fill-col-mid transition ease-in-out md:size-8 lg:size-9"
+            :class="svg.hoverColour"
+            target="_blank"
+        >
             <component :is="svg.component" />
         </NuxtLink>
     </div>
